5115346902ccc4bae746567e8dad7e32d31d2567
Kwazi <kwazinkosizwane@gmail.com>
2025-03-10 01:21:01 +0200

    Fixed parallel execution

--- src/test/java/listeners/ReportListener.java
+++ src/test/java/listeners/ReportListener.java
@@ -3,6 +3,9 @@ package listeners;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
@@ -13,9 +16,16 @@ import utils.DriverFactory;
 import utils.ScreenshotUtil;
 
 public class ReportListener implements ITestListener {
-    private static final ExtentReports extent = ExtentManager.createInstance();
+    
+	private static final ExtentReports extent = ExtentManager.createInstance();
     private static final ThreadLocal<ExtentTest> test = new ThreadLocal<>();
-
+    private static long startTime;
+    
+    @Override
+    public void onStart(ITestContext context) {
+        startTime = System.currentTimeMillis();
+    }
+    
     @Override
     public void onTestStart(ITestResult result) {
         // Create a new ExtentTest instance and set it in ThreadLocal
@@ -58,7 +68,34 @@ public class ReportListener implements ITestListener {
 
     @Override
     public void onFinish(ITestContext context) {
-        // Flush the ExtentReports instance
+    	long totalTime = System.currentTimeMillis() - startTime;
+        String formattedTime = formatDuration(totalTime);
+
+        // Add system info
+        extent.setSystemInfo("Total Execution Time", formattedTime);
+        extent.setSystemInfo("Passed Tests", String.valueOf(context.getPassedTests().size()));
+        extent.setSystemInfo("Failed Tests", String.valueOf(context.getFailedTests().size()));
+        extent.setSystemInfo("Skipped Tests", String.valueOf(context.getSkippedTests().size()));
+
+        // Custom summary table
+        String[][] summary = {
+            {"Total Tests", String.valueOf(context.getAllTestMethods().length)},
+            {"Passed", String.valueOf(context.getPassedTests().size())},
+            {"Failed", String.valueOf(context.getFailedTests().size())},
+            {"Skipped", String.valueOf(context.getSkippedTests().size())},
+            {"Execution Time", formattedTime}
+        };
+
+        extent.createTest("Execution Summary")
+              .info(MarkupHelper.createTable(summary));
+
         extent.flush();
+    }
+
+    private String formatDuration(long millis) {
+        long hours = TimeUnit.MILLISECONDS.toHours(millis);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis) % 60;
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis) % 60;
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
 }
